30a85164ec5540858f7edef75d3710645f1fbc28
hareemzaf <150966998+hareemzaf@users.noreply.github.com>
2023-12-09 19:38:27 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+// console.log("assalam walikum");
+// console.log("hello hareem");
+// // example
+// var powder = "Lal mirch powder";
+// console.log("hello");
+// console.log(powder);
+// powder = "Haldi powder";
+// console.log(powder);
+// // this is my second class
+// // some example
+// var fullName = "hareem";
+// // full name
+// var firstname = "hareem";
+// var lastname = "zafar";
+// // console.log(firstname + "" + lastname);
+// console.log("my name is ".concat(firstname, " ").concat(lastname));
+// console.log("hello world");
+// //add
+// var qty = 2;
+// console.log(qty + 10);
+// //sub
+// console.log(qty - 1);
+// //multiply
+// console.log(qty * 12000);
+// //divide
+// console.log(qty / 3);
+// //
+// var age = 22;
+// age = 10;
+// console.log(age);
+// var n = 5;
+// console.log(n); // 5
+// n += 5;
+// console.log(n); // 10
+// n -= 5;
+// console.log(n); // 5
+// var number = 5;
+// console.log(n == 5); //
+// console.log(n === 5); //
+// console.log(n != 5); //
+// console.log(n > 8); //
+// console.log(n < 8); //
+// console.log(n >= 8); //
+// console.log(n <= 8); // 
---
+++ main.ts
@@ -0,0 +1,117 @@
+// console.log("assalam walikum");
+// console.log("hello hareem");
+
+// // example
+
+// let powder = "Lal mirch powder";
+
+// powder = "Haldi";
+
+// console.log("Hello");
+
+// console.log(powder);
+
+
+// let fullName:string = "hareem";
+
+// fullName = "hifza"
+// fullName = "hiba"
+// fullName = "hareem"
+
+// console.log(fullName);
+
+
+// let fullName = "hareem zafar";
+// let age = 18;
+
+
+// let bulb = true;
+// let bulb = false;
+
+
+
+// let fullName;
+
+// console.log(fullName);
+
+// fullName = "hareem" 
+
+
+// let firstName = "hareem";
+// let lastName = "zafar";
+
+
+// // console.log(firstName + " " + lastName);
+// console.log(`my name is ${firstName} ${lastName}`);
+
+
+// let qty = 8;
+//add
+// console.log(qty + 10);
+// sub
+// console.log(qty - 1);
+// multiply
+// console.log(qty * 12000);
+// divide
+// console.log(qty / 3);
+
+
+// let str1 = "1";
+// let str2 = "2";
+// console.log(str1 + str2); 
+
+// // let n1 = 2;
+// // let n2 = 3;
+
+// // console.log(n1 ** n2)
+
+// console.log(qty % 3)
+
+
+
+// let age = 18;
+
+// // age += 10;
+// age -= 10;
+
+// console.log(age);
+
+
+// let n = 5;
+// console.log(n); // 5
+
+// n += 5;
+// console.log(n); // 10
+
+// n -= 5;
+// console.log(n); // 5
+
+
+
+
+// let age = 22;
+
+// console.log( age == 22 );
+// console.log( age === '22' );
+// console.log(age != 22);
+// console.log(age > 10);
+// console.log(age >= 22);
+// console.log(age <= 22);
+
+
+
+
+// let n = 5;
+// console.log(n == 5); // True
+// console.log(n === 5); // True
+// // console.log(n != 5); // False
+// // console.log(n > 8); // False
+// console.log(n < 5); // False
+// // console.log(n >= 8); // Flase
+// console.log(n <= 8); // True
+
+
+
+
+
+
